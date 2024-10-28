afdb7ae759d21c250339085eff514c9d2deaaa49
Aleksandr Kutsybala <41694302+Grekerochek@users.noreply.github.com>
2024-10-28 22:37:22 +0300

    Initial commit

--- app/src/main/java/com/example/phenakistiscope/MainScreenContent.kt
+++ app/src/main/java/com/example/phenakistiscope/MainScreenContent.kt
@@ -18,8 +18,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
